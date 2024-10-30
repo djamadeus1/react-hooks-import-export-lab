4fe08c82eaaf382c2e9367b1f66a21d8000add91
Ronald Roberts <djamadeus1@gmail.com>
2024-10-29 21:06:18 -0400

    very confusing

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import About from "./components/About";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
